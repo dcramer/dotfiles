bbebc4137a557e3532afb732bd1b748b04151e4e
David Cramer <dcramer@gmail.com>
2011-07-05 11:12:42 -0700

    Added pythonrc (inspired by donri)

---
+++ python/pythonrc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Inspired by https://github.com/dag/dotfiles/blob/master/python/.pythonrc
+
+import atexit
+import os
+import readline
+import rlcompleter
+
+readline.parse_and_bind('tab: complete')
+history = os.path.expanduser("~/.pythonhist")
+
+def save_history(history=history):
+    import readline
+    readline.write_history_file(history)
+
+if os.path.exists(history):
+    readline.read_history_file(history)
+
+atexit.register(save_history)
+del os, atexit, readline, rlcompleter, save_history, history
