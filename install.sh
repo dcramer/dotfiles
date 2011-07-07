f3a6261860d0cb2140e47da645edbfc808dfacac
David Cramer <dcramer@gmail.com>
2011-07-07 15:31:10 -0700

    Add some output for install script

--- install.sh
+++ install.sh
@@ -2,12 +2,18 @@
 
 if [ `uname` == "Darwin" ]; then
   if [ -e "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ]; then
+    echo "Installing 'subl' command"
     ln -fs "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
   fi
 fi
 
 mkdir -p ~/.virtualenvs/
 
+echo "Installing .bash_profile"
 ln -fs `pwd`/bash/bashrc ~/.bash_profile
+
+echo "Installing virtualenvwrapper hooks"
 ln -fs `pwd`/virtualenvwrapper/* ~/.virtualenvs/
+
+echo "Installing .pythonrc.py"
 ln -fs `pwd`/dotfiles/python/pythonrc.py ~/.pythonrc.py
