9d00ef908c1d6982ec29e8bb3da620d5255fe9c1
David Cramer <dcramer@gmail.com>
2014-08-08 22:09:42 -0700

    Switch to oh-my-fish + edan theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,20 @@
-set path (dirname (status -f))
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme edan
+
+# Enable plugins
+set fish_plugins git brew python sublime rvm node
 
 # virtualfish
+set vfishpath (dirname (status -f))
+
 set -g VIRTUALFISH_COMPAT_ALIASES
-. $path/virtualfish/virtual.fish
-. $path/virtualfish/auto_activation.fish
-. $path/virtualfish/global_requirements.fish
-. $path/virtualfish/projects.fish
+. $vfishpath/virtualfish/virtual.fish
+. $vfishpath/virtualfish/auto_activation.fish
+. $vfishpath/virtualfish/global_requirements.fish
+. $vfishpath/virtualfish/projects.fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
